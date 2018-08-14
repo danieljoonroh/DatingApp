96c9e8895d723870e093f0bd6ac8926f2ab49ca1
Daniel Roh <danieljoonroh@Daniels-MacBook-Pro.local>
2018-08-14 10:58:54 -0700

    added route resolvers and photo gallery

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -16,7 +16,7 @@ namespace DatingApp.API.Helpers
         {
             var age = DateTime.Today.Year - theDateTime.Year;
             if (theDateTime.AddYears(age) > DateTime.Today)
-                 age --;
+                age--;
 
             return age;
         }
